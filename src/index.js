d30c290daedcd332e37e9d8cba10f3b4a651d959
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2022-07-31 19:21:49 +0200

    try to make my home work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@
  * true, если есть различие, false. То есть проверяет каждое свойство, вне зависимости от вложенности,
  * делаем через рекурсию(а других вариантов и нет)
  */
-export const deepEqual = (obj, anotherObject) => {};
+export const deepEqual = (obj, anotherObject) => {
+  if (typeof obj === 'object' && typeof anotherObject === 'object') {
+    for (let [key, value] of Object.entries(obj)) {
+      for (let [key2, value2] of Object.entries(anotherObject)) {
+        if (typeof value === 'object' || typeof value2 === 'object') {
+          return deepEqual([key, value], [key2, value2]);
+        } else if (key === key2 && value === value2) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  } else {
+    return obj === anotherObject;
+  }
+};
 
 /**
  * Принимает объект, возвращает его глубокую копию, то есть ни одно свойство
@@ -12,10 +28,35 @@ export const deepEqual = (obj, anotherObject) => {};
  * то тогда в рекурсию. С объектом также. Поскольку массив при typeof возвращает object, чтобы
  * их различить берем метод Array.isArray и он на массивах вернет тру
  */
-export const deepCopy = (obj) => {};
+export const deepCopy = (obj) => {
+  if (typeof obj !== 'object' && !Array.isArray(obj)) {
+    return obj;
+  } else if (Array.isArray(obj)) {
+    return obj.map((value) => {
+      return deepCopy(value);
+    });
+  } else if (typeof obj === 'object') {
+    let result = {};
+    for (let x in obj) {
+      result[x] = deepCopy(obj[x]);
+    }
+    return result;
+  }
+};
 
 /**
  * Мы передаем объект, и должны вернуть массив уникальных названий свойств
  * То есть если у нас объект { name: { bohdan: { name: 'test' } } } вернет ['name', 'bohdan']
  */
-export const getAllObjectKeys = (obj) => {};
+export const getAllObjectKeys = (obj) => {
+  return Object.entries(obj).reduce((accum, item) => {
+    let result = '';
+    if (typeof item[1] === 'object') {
+      result = [...accum, item[0], ...getAllObjectKeys(item[1])];
+      return [...new Set(result)];
+    } else {
+      result = [...accum, item[0]];
+      return [...new Set(result)];
+    }
+  }, []);
+};
